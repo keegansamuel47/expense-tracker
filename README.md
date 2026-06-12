d06186a9a4905b00927aba04d88396a093390d0f
Keegan Samuel <keegansamuel47@gmail.com>
2026-06-12 22:47:43 +0100

    add expense tracker

---
+++ README.md
@@ -0,0 +1,1 @@
+This project is about creating an expense tracker 
---
+++ tracker.py
@@ -0,0 +1,3 @@
+expenses = input("what did you spend money on?")
+price = float(input( "how much"))
+print(f"Recorded: {expenses } - ${price:.2f}")
