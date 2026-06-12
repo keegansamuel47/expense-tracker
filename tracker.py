e9e744c25aec6595ca121d71daabec8b151cf980
Keegan Samuel <keegansamuel47@gmail.com>
2026-06-13 00:49:08 +0100

    fix trailing space and input arrangement

--- tracker.py
+++ tracker.py
@@ -1,8 +1,9 @@
-expense = input("what did you spend money on?")
-second_expense = input("what else did you spend on?")
-price = float(input("how much"))
-second_price = float(input("how much for the second expense?"))
+expense = input("what did you spend money on? ")
+price = float(input("how much "))
+expense_2 = input("what else did you spend on? ")
+second_price = float(input("how much for the second expense? "))
 
+print(f"Recorded: {expense} - ${price:.2f}")
 print(f"Total spent: ${price + second_price:.2f}.")
 
 
