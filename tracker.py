6081196aaa283463edf365b7ec851320c03df2ea
Keegan Samuel <keegansamuel47@gmail.com>
2026-06-13 00:29:58 +0100

    Add exercise on f string and README

--- tracker.py
+++ tracker.py
@@ -1,3 +1,33 @@
-expenses = input("what did you spend money on?")
-price = float(input( "how much"))
-print(f"Recorded: {expenses } - ${price:.2f}")
+expense = input("what did you spend money on?")
+second_expense = input("what else did you spend on?")
+price = float(input("how much"))
+second_price = float(input("how much for the second expense?"))
+
+print(f"Total spent: ${price + second_price:.2f}.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
