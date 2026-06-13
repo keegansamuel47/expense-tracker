17a4ae3fa2f50a849cdf9e2fea5ae3c16712c922
Keegan Samuel <keegansamuel47@gmail.com>
2026-06-13 01:04:21 +0100

    Fix spelling and new line break

--- tracker.py
+++ tracker.py
@@ -4,5 +4,5 @@ expense_2 = input("what else did you spend on? ")
 second_price = float(input("how much for the second expense? "))
 
 print(f"Recorded: {expense} - ${price:.2f}")
-print(f"Recoded: {expense_2} - ${second_price:.2f}")
+print(f"Recorded: {expense_2} - ${second_price:.2f}")
 print(f"Total spent: ${price + second_price:.2f}.")
