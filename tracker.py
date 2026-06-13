228506901370a7584ac4971f48323d9c7e9ef9f7
Keegan Samuel <keegansamuel47@gmail.com>
2026-06-13 02:38:19 +0100

    Add conditional statement

--- tracker.py
+++ tracker.py
@@ -1,8 +1,13 @@
+
 expense = input("what did you spend money on? ")
 price = float(input("how much? "))
 expense_2 = input("what else did you spend on? ")
 second_price = float(input("how much for the second expense? "))
 
-print(f"Recorded: {expense} - ${price:.2f}")
-print(f"Recorded: {expense_2} - ${second_price:.2f}")
-print(f"Total spent: ${price + second_price:.2f}.")
+if price <= 0 or second_price <= 0:
+   print("That's not a valid amount.")
+else:
+   print(f"Recorded: {expense} - ${price:.2f}")
+   print(f"Recorded: {expense_2} - ${second_price:.2f}")
+   print(f"Total spent: ${price + second_price:.2f}.")
+
