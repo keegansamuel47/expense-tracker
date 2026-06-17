7e022fea04a365a919cdaf9747192df11ba3f3a8
Keegan Samuel <keegansamuel47@gmail.com>
2026-06-17 16:27:08 +0100

    Fix total spent

--- tracker.py
+++ tracker.py
@@ -4,22 +4,21 @@ price = float(input("how much? "))
 expense_2 = input("what else did you spend on? ")
 second_price = float(input("how much for the second expense? "))
 
-
+total= 0
 if price > 0:
     print(f"Recorded: {expense} - ${price:.2f}")
+    total += price
 else:
     print("That's not a valid amount.")
 
 if second_price > 0:
     print(f"Recorded: {expense_2} - ${second_price:.2f}")
+    total += second_price
 else:
     print("That's not a valid amount.")
 
+print(f"Total spent: ${total:.2f}.")
+
 #in order to get the lines to print the total with respect to their independent prices , use a connecting statement , if price is true and second price is false print.... so
 #it checks for the true or false statement and print
-if price > 0 >= second_price:
-    print(f"Total spent: ${price:.2f}")
-elif price <= 0 < second_price:
-    print(f"Total spent: ${second_price}")
-else:
-    print(f"Total spent: ${price + second_price:.2f}.")
+
